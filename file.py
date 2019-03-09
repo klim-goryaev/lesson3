cf3b2d75f41e7d7e6fba888b3b109f3708edcc83
Горяев Клим Сергеевич <klim.goryaev@rtlabs.ru>
2019-03-09 12:48:04 +0300

    Исправления и улучшения

--- file.py
+++ file.py
@@ -3,17 +3,10 @@ with open('referat.txt', 'r', encoding='utf-8') as referat:
     print("Длина контента составляет: ", len(content), "символов")#считаем длину контента всего
 
 with open('referat.txt', 'r', encoding='utf-8') as referat_slova:#считаем длину пробелов, как разделителей между словами.
-    for bez_prep in referat_slova:
-        bez_prep = bez_prep.replace ("\n","")
-        str_prob = int(len(bez_prep.split (' ')))#считаем количество пробелов
-        sum_prob = 0
-        sum_prob += sum(bez_prep)
-        print(sum_prob)#выводим построчно пробелы
-       
+    content = referat_slova.read()
+    print("Длина контента составляет: ", len(content.split(" ")), "слов")#считаем длину контента всего
 
-        
-
-
+     
 with open('referat.txt', 'r', encoding='utf-8') as referat_voskl:
     for voskl in referat_voskl:
         voskl = voskl.replace (".","!")#заменяем точки на восклицательные знаки
--- time.py
+++ time.py
@@ -1,17 +1,17 @@
-import datetime  #импорт модуля времени
-import timedelta #импорт модуля timedelta
-import locale  #импорт модуля locale 
+import datetime
+import locale  
+import calendar 
 locale.setlocale(locale.LC_ALL, "russian")
 from datetime import datetime #импортируем datetime
 from datetime import timedelta #импортируем timedelta
-dt_now = datetime.now() #присваиваем 
-delta_day = timedelta(days=1)
-dt_yesterday = (dt_now-delta_day)
-delta_month = timedelta(days=30)
-dt_last_month = (dt_now-delta_month)
-print ('Дата вчера: ',dt_yesterday.strftime('%A %d.%m.%Y')) #дата вчера
-print ("Дата сегодня: ",dt_now.strftime('%A %d.%m.%Y')) #дата сейчас
-print ("Дата месяц назад: ",dt_last_month.strftime('%A %d.%m.%Y')) #дата месяц назад
+dt_yesterday = (datetime.now()-timedelta(days=1))#формула для вчера
+dt_last_month = datetime.now()
+days_in_month = calendar.monthrange(dt_last_month.year, dt_last_month.month)[1]
+dt_last_month -= timedelta(days=days_in_month) #формула для месяц назад
+a=('%A %d.%m.%Y') #объявляем одинаковые значения для формата
+print ('Дата вчера: ',dt_yesterday.strftime(a)) #дата вчера
+print ("Дата сегодня: ",datetime.now().strftime(a)) #дата сейчас
+print ("Дата месяц назад: ",dt_last_month.strftime(a)) #дата месяц назад
 date_string = '01/01/17 12:10:03.234567'#stirng из примера
 date_dt = datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f') #приводим к распознованию
 print ("Полное время: ", date_dt) #показываем полную дату
